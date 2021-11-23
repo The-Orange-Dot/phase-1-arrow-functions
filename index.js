0748ae4715814c0bb6c287c2fa9b38868bc982fa
TheOrangeDot <theorangedottwitch@gmail.com>
2021-11-23 12:38:55 +0900

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+function divide() {
+  return (2000 / 100);
+}
+
+const square = num => num * num;
+
+const add = (a,b) => a+b;
+
+console.log(divide())
+console.log(square(5))
+console.log(add(2,3))
